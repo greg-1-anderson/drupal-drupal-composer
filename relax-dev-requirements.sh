e66c69398cfb4b8d454706a3d31dccb42d04ccfd
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-08-28 14:50:01 -0700

    Start using official drupal/core-recommended, drupal/pinned-dev-dependencies and drupal/dev-dependencies. Temporarily switch to using unofficial greg-1-anderson/core-vendor-hardening until issue with drupal/core-file-security is resolved.

--- relax-dev-requirements.sh
+++ relax-dev-requirements.sh
@@ -2,6 +2,6 @@
 
 VERSION_CONSTRAINT="${1:-8.8.x-dev}"
 
-composer remove --dev --no-update greg-1-anderson/core-recommended-dev-dependencies
-composer require --dev "greg-1-anderson/core-dev-dependencies:$VERSION_CONSTRAINT"
+composer remove --dev --no-update drupal/pinned-dev-dependencies
+composer require --dev "drupal/dev-dependencies:$VERSION_CONSTRAINT"
 composer update --lock
